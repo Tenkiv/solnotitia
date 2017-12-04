c81ad050c22cf7c54b5ad3059ea76b8a872747de
Shan <skern@tenkiv.com>
2017-12-04 11:30:11 -0800

    wiped javafx stuff; starting project with jmonkey

--- src/main/kotlin/com/tenkiv/support/solnotitia/Main.kt
+++ src/main/kotlin/com/tenkiv/support/solnotitia/Main.kt
@@ -1,7 +1,5 @@
 package com.tenkiv.support.solnotitia
 
-import javafx.application.Application
-
 fun main(args: Array<String>) {
-    Application.launch(SolNotitia::class.java, *args)
+    //
 }
--- src/main/kotlin/com/tenkiv/support/solnotitia/views/HomeSystemView.kt
+++
@@ -1,9 +0,0 @@
-package com.tenkiv.support.solnotitia.views
-
-import tornadofx.View
-import tornadofx.stackpane
-
-class HomeSystemView : View() {
-    override val root = stackpane {
-    }
-}
